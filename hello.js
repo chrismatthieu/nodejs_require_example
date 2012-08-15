c104dfa4703647759cd5b58c8951cc3e92c7e302
Chris Matthieu <chris@matthieu.us>
2012-08-14 19:41:07 -0700

    added require example

--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var test = require('./test.js');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8064, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8064/');
+  res.end('Hello ' + test.opt.message);
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
---
+++ test.js
@@ -0,0 +1,5 @@
+exports.opt = {
+  message: 'Chris'
+};
+
+
